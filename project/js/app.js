5343e2a6107812dc621cb4f944a3f507ede41c0f
Aryan Kumar Yadav <ay3978943@gmail.com>
2023-11-11 14:09:31 +0530

    document

--- project/js/app.js
+++ project/js/app.js
@@ -10,10 +10,34 @@ $(document).ready(function(){
     smartSpeed: 1000,
     responsive:{
         0:{
-          
+          nav: false, 
         },
-        600:{
-         
+        768:{
+         nav:true,
+        },
+        1000:{
+          
+        }
+    }
+})
+
+ //projectslider
+
+
+$('#hero-slider').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    items: 1,
+    navText: ['PREV', 'NEXT'],
+    dots:false,
+    smartSpeed: 1000,
+    responsive:{
+        0:{
+          nav: false, 
+        },
+        768:{
+         nav:true,
         },
         1000:{
           
